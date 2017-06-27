d4107baff376a54bd56f0c922297df8508372cac
DanyalS <danyal.6ix@gmail.com>
2017-06-27 12:06:33 -0400

    Added all email editor files

---
+++ server-config.js
@@ -0,0 +1,12 @@
+// Put here your email sending configurations!
+module.exports = {
+	emailTransport: {
+		host: 'mail.smtp2go.com',
+		port: 2525,
+		auth: { user: 'danyal.shakeel@gmail.com', pass: 'dZvyDYgTaXYv' }
+	},
+	emailOptions: {
+		from: 'Email Editor Test Email', // sender address
+		//bcc: 'mosaico@mosaico.io',
+	}
+};
